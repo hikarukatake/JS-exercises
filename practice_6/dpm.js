0e4b8959545545f87a19f42ce45ab055f72ac785
hikaru <h.matsuba.sys23@morijyobi.ac.jp>
2024-03-25 17:32:41 +0900

    DOM操作①-2最終課題

---
+++ practice_6/dpm.js
@@ -0,0 +1,33 @@
+`use strict`;
+
+function click1() {
+    let wordValue = document.getElementById("wordInput").value;
+    let result = document.getElementById("result");
+    result.textContent = wordValue;
+    result.style.cssText = "color: black; font-size:15px";
+}
+function click2() {
+    let wordValue = document.getElementById("wordInput").value;
+    let result = document.getElementById("result");
+    result.textContent = wordValue;
+    result.style.cssText = "color: orange; font-size:100px";
+}
+function click3() {
+    let wordValue = document.getElementById("wordInput").value;
+    let result = document.getElementById("result");
+    result.textContent = wordValue;
+    result.style.cssText = "color: green; font-size:30px";
+}
+function click4() {
+    let wordValue = document.getElementById("wordInput").value;
+    let result = document.getElementById("result");
+    result.textContent = wordValue;
+    result.style.cssText = "color: blue; font-size:75px";
+}
+function click5() {
+    let wordValue = document.getElementById("wordInput").value;
+    let result = document.getElementById("result");
+    result.textContent = wordValue;
+    result.style.cssText = "color: red; font-size:50px";
+}
+
