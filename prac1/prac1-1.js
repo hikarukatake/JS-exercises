db4611d705fc24ff5edd8a1311ee94178e6931f1
hikaru <h.matsuba.sys23@morijyobi.ac.jp>
2024-03-26 14:28:12 +0900

    変更の修正

--- prac1/prac1-1.js
+++ prac1/prac1-1.js
@@ -1,18 +1,20 @@
 `use strict`
-height = window.prompt("あなたの身長は何センチ？");
-if (height >= 100) {
-    ticket = window.confirm("プレミアムチケットを持っていますか？");
-} if (height >= 90) {
-    window.alert("保護者同伴ならいける");
-} if (height = "") {
-    window.alert("入力がないと判定できません")
-} if (height.match(/^[0-9]+$/)) {
+let height = window.prompt("あなたの身長は何センチ？");
+if (height == "") {
+    window.alert("文字を入力して")
+} else if (!height.match(/^[0-9]+$/)) {
+    window.alert("半角数値で入力してください")
+}
+else if (height >= 100) {
+    ticket = window.confirm("プレミアムチケットを持っていますか？")
+    if (ticket === true) {
+        window.alert("プレミアムシートに乗車可能です。")
+    } else {
+        window.alert("通常のシートに乗車可能です。")
+    }
+}else if (height >= 90) {
+    window.alert("親同伴なら乗車可能です")
 } else {
-    window.alert("半角数値でかきなさい")
+    window.alert("乗車できません")
 }
 
-if (ticket === true) {
-    window.alert("プレミアムシートに乗車可能です。")
-} else {
-    window.alert("通常のシートに乗車可能です。")
-}
